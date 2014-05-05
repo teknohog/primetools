d2f061e1b3256c37b8fdc01c576cf2d2498de06c
Risto A. Paju <teknohog@iki.fi>
2014-05-05 21:23:36 +0300

    Safer defaults for cllucas.sh

--- cllucas.sh
+++ cllucas.sh
@@ -2,16 +2,17 @@
 
 # by teknohog
 
-# A more advanced example of running clLucas and lliki.py in a
-# continuous loop.
+# Run clLucas and lliki.py in a continuous loop.
 
 # Please check this script carefully before use, as the optimal
 # settings (THREADS, aticonfig) depend on the GPU, and there are other
 # custom settings like the working directory and LD_LIBRARY_PATH.
 
-BASEOPTS="-u user -p pass"
-WORKOPTS="-n 2"
+BASEOPTS="-u teknohog -p inhermajestyssecretcervix"
+WORKOPTS="-n 1 -T 100"
 FLUSH=false
+ONESHOT=false
+PROG=clLucas
 
 # 2097152 and 4194304 fft work best.(-f)
 # http://mersenneforum.org/showpost.php?p=350303&postcount=64
@@ -22,42 +23,76 @@ function set_fft () {
 	THREADS=128
     else
 	FFT=2097152
-	THREADS=256
+	THREADS=64
     fi
 }
 
-while getopts f opt; do
+while getopts fo opt; do
     case $opt in
         f)
             FLUSH=true
             ;;
+	o)
+	    ONESHOT=true
+	    ;;
     esac
 done
 
-# Finish current work without getting any new
 if $FLUSH; then
     WORKOPTS="-n 0"
 fi
 
-cd distr.projects/clLucas.1.0/
+#cd ~/distr.projects/clLucas.1.01
+cd ~/distr.projects/clLucas.1.02/bin/x86_64/Release
 
 export LD_LIBRARY_PATH=~/sources/clFFT/src/library/
 
 # Device numbering as in cgminer
-DEVICE=0
-aticonfig --adapter=0 --odsc=1000,1500
+# GB 7970
+DEVICE=1
+aticonfig --adapter=0 --odsc=1050,1500
+
+# PCS+5870
+#DEVICE=0
+#aticonfig --adapter=0 --odsc=975,1200
+
+# Speed test with known M#48
+#MERSENNE=57885161
+
+# Small tests of known M#
+#MERSENNE=110503
+
+# Avoid running twice on the same GPU
+if [ -n "`ps aux | grep $PROG | grep \" -d $DEVICE\"`" ]; then
+    echo $PROG already running on device $DEVICE.
+    exit
+fi
+
+if $ONESHOT; then
+    # Test an individual M# without looping, useful for testing and
+    # benchmarking before production runs
+    
+    MERSENNE=58818269
+    #MERSENNE=32163559
+    set_fft
+    ./$PROG -aggressive -d $DEVICE -f $FFT -threads $THREADS $MERSENNE
+    #lliki.py $BASEOPTS -n 0
+    exit
+fi
 
 while true; do
-    # Quick file operations first...
-    eval $(lliki.py -N)
+    # Basic, recommended operation
+    eval $(lliki.py $BASEOPTS $WORKOPTS)
 
-    # ...then leave networking in the background.
-    lliki.py $BASEOPTS $WORKOPTS -d &
+    # Advanced alternative: Quick file operations first, then handle
+    # networking in the background.
+    #eval $(lliki.py -N)
+    #lliki.py $BASEOPTS $WORKOPTS -d &
     
     set_fft
 
     if [ -n "$MERSENNE" ]; then
-	./clLucas -aggressive -d $DEVICE -f $FFT -threads $THREADS $MERSENNE
+	./$PROG -aggressive -d $DEVICE -f $FFT -threads $THREADS $MERSENNE
     else
 	break
     fi
