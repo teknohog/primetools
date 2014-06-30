02e6d3bafc48c22186ea51b2c46fc8fbdd3a39f2
k3ack3r <pcexplorer12@gmail.com>
2014-06-30 10:10:04 -0700

    integrated mfloop options

--- mfaktx.bat
+++ mfaktx.bat
@@ -1,28 +1,42 @@
 @echo off
 
 REM expirimental/developing mfloop "automation" script on windows
+REM Time to wait between reboots if needed. 
+set waittime=60
+
 REM mfloop settings
 REM
 REM Primenet username
-set PrimenetUsername=
+set PrimenetUsername=test
 
 REM Primenet password
-set PrimenetPassword=
+set PrimenetPassword=test123
+
+REM How much work to get: If using gpu72, use GHzdays. If not, number of assignments.
+set cache=100
 
 REM Use gpu72 0/no 1/yes
 set UseGpu72=1
 
-REM ###
 REM Arguments to mfakto
-set mvar=
+set mvar=-d 1
 
 REM Executable name
-set exec=
-:run
+set exec=mfakto.exe
+
+
+
+:crunch
 %exec% %mvar%
+timeout /T %waittime% > NUL
 goto run
 
+:setservice
+title mfloop service
+set mfloop_arg=--username %PrimenetUsername% --password=%PrimenetPassword% -w %cd%
+IF %UseGpu72%==1 set mfloop_arg=%mfloop_arg% --gpu72user=%gpu72user% --gpu72pass=%gpu72pass% 
+IF %UseGpu72%==1 set mfloop_arg=%mfloop_arg% --ghzd_cache=%cache% ELSE set mfloop_arg=%mfloop_arg% --num_cache=%cache%
 :service
 python.exe mfloop.py %mfloop_arg%
+timeout /T %waittime% > NUL
 goto service
-
