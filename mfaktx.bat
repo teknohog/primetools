ee2e0c4f992092474ae00a40883301949bf1f2a7
k3ack3r <pcexplorer12@gmail.com>
2014-06-30 10:25:07 -0700

    something actually working

--- mfaktx.bat
+++ mfaktx.bat
@@ -1,4 +1,3 @@
-@echo off
 
 REM expirimental/developing mfloop "automation" script on windows
 REM Time to wait between reboots if needed. 
@@ -18,24 +17,29 @@ set cache=100
 REM Use gpu72 0/no 1/yes
 set UseGpu72=1
 
+REM GPU72 settings
+set gpu72user=test
+set gpu72pass=test123
+
 REM Arguments to mfakto
 set mvar=-d 1
 
 REM Executable name
 set exec=mfakto.exe
 
-
-
+:start
+if %1:==-s: goto setservice
+start mfaktx.bat -s
 :crunch
 %exec% %mvar%
 timeout /T %waittime% > NUL
-goto run
+goto crunch
 
 :setservice
 title mfloop service
 set mfloop_arg=--username %PrimenetUsername% --password=%PrimenetPassword% -w %cd%
 IF %UseGpu72%==1 set mfloop_arg=%mfloop_arg% --gpu72user=%gpu72user% --gpu72pass=%gpu72pass% 
-IF %UseGpu72%==1 set mfloop_arg=%mfloop_arg% --ghzd_cache=%cache% ELSE set mfloop_arg=%mfloop_arg% --num_cache=%cache%
+REM IF %UseGpu72%==1 set mfloop_arg=%mfloop_arg% --ghzd_cache=%cache%. ELSE set mfloop_arg=%mfloop_arg% --num_cache=%cache%.
 :service
 python.exe mfloop.py %mfloop_arg%
 timeout /T %waittime% > NUL
