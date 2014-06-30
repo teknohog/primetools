01c6849724e7c11a0a32cff3abf9b457afd6c875
k3ack3r <pcexplorer12@gmail.com>
2014-06-30 08:46:18 -0700

    working...

--- mfaktx.bat
+++ mfaktx.bat
@@ -1,6 +1,16 @@
 @echo off
 
 REM expirimental/developing mfloop "automation" script on windows
+REM mfloop settings
+REM
+REM Primenet username
+set PrimenetUsername=
+
+REM Primenet password
+set PrimenetPassword=
+
+REM Use gpu72 0/no 1/yes
+set UseGpu72=1
 
 REM ###
 REM Arguments to mfakto
@@ -11,4 +21,8 @@ set exec=
 :run
 %exec% %mvar%
 goto run
+
+:service
+python.exe mfloop.py %mfloop_arg%
+goto service
 
