037508d4c63562bf7474b11d31f19d477b386392
k3ack3r <pcexplorer12@gmail.com>
2014-07-03 11:51:49 +0900

    selfevident rem removed

--- mfaktx.bat
+++ mfaktx.bat
@@ -2,26 +2,14 @@
 REM Time to wait between reboots if needed. 
 set waittime=120
 
-REM ### mfloop settings
-REM ###
-REM Primenet username
 set PrimenetUsername=test
-
-REM Primenet password
 set PrimenetPassword=test123
-
 REM How much work to get: If using gpu72, use GHzdays. If not, number of assignments.
 set cache=100
-
-REM ### GPU72 settings
-REM ###
-REM Use gpu72 0/no 1/yes
-
+REM gpu72 0/no 1/yes
 set UseGpu72=1
 set gpu72user=test
 set gpu72pass=test123
-
-REM Type of work from GPU72 to get, lltf or dctf
 set gpu72_type=lltf
 
 REM GPU72 fetch options:
@@ -29,7 +17,7 @@ REM GPU72 Option to fetch, default what_makes_sense. Other valid values are lowe
 REM oldest_exponent, no_p1_done (dctf only), lhm_bit_first (lltf only), lhm_depth_first (lltf only), and let_gpu72_decide (let_gpu72_decide may override max_exp)
 set gpu72_option=what_makes_sense
 
-REM Arguments to mfakto
+REM Arguments to mfaktx
 set mvar=-d 1
 
 REM Executable name
@@ -44,7 +32,7 @@ if %1:==-s: goto setservice
 start mfaktx.bat -s
 :crunch
 %exec% %mvar%
-echo ERROR: %exec% unexpectedly quit, waiting %waittime% seconds to restart...
+echo ERROR: %exec% unexpectedly quit or ranout of work, waiting %waittime% seconds to restart...
 timeout /T %waittime% > NUL
 cls
 goto crunch
