d5ddc6de2eaf37c6c448ebe90a61231be6e3cc53
k3ack3r <pcexplorer12@gmail.com>
2014-09-18 16:53:27 -0700

    Reverted (only for now) to "single" mode for..
    merge request..plus i'm not sure if it's worth having.

--- mfloop.bat
+++ mfloop.bat
@@ -24,41 +24,17 @@ set mvar=-d 1
 REM Executable name
 set exec=mfakto.exe
 
-
-REM Multi-clients
-REM NOTICE: The first client does not count, use the second client's path in 2, and so on. Settings will be same for every client.
-set MultipleClients=1
-set Clients=3
-set 2=C:\1
-set 3=C:\2
 REM ###END OF SETTINGS###
 
 :check
-IF %MultipleClients%==1 set dir=%~1 ELSE set dir=%cd% 
 IF NOT EXIST mfloop.py echo Error: mfloop.py could not be found, press any key to exit&&pause >NUL &&goto exit
 IF NOT EXIST %exec% echo Error: %exec% could not be found, press any key to exit&&pause >NUL &&goto exit
-if %1:==-s: goto setservice
-:check_multi
-IF %round%==1 goto start
-IF %MultipleClients%==1 goto multi else goto start
-echo Warning: Unknown input for %MultipleClients%, disabling.  
-goto start
-
-:multi
-REM Replace with "for" later
-set round=1
-goto %clients%
-:3
-start mfloop.bat %3%
-:2
-start mfloop.bat %2%
-:1
-echo Warning: Only 1 client set.
 
 :start
+if %1:==-s: goto setservice
 start mfaktx.bat -s
 del *.lck /F/Q
-title %dir%\%exec%
+title %cd%\%exec%
 :check
 cls
 IF NOT EXIST worktodo.txt echo ERROR: worktodo.txt not found, waiting for work...&&timeout /T 3 >NUL&&goto check
@@ -71,7 +47,7 @@ goto crunch
 
 :setservice
 title mfloop service
-set mfloop_arg=--username %PrimenetUsername% --password=%PrimenetPassword% -w %dir% --timeout=%waittime% -e %max_bit% -d --ghzd_cache=%WorkCache%
+set mfloop_arg=--username %PrimenetUsername% --password=%PrimenetPassword% -w %cd% --timeout=%waittime% -e %max_bit% -d --ghzd_cache=%WorkCache%
 REM IF %UseGPU72%==1 goto sGPU72 ELSE goto service
 :sGPU72
 IF %UseGpu72%==1 set mfloop_arg=%mfloop_arg% --gpu72user=%gpu72user% --gpu72pass=%gpu72pass% --gpu72type=%gpu72_type% --gpu72option=%gpu72_option%
@@ -80,7 +56,7 @@ IF %UseGpu72%==1 set mfloop_arg=%mfloop_arg% --gpu72user=%gpu72user% --gpu72pass
 cls
 title Mfloop service
 echo mfloop service
-echo running at: %dir%
+echo running at: %cd%
 python.exe mfloop.py %mfloop_arg%
 echo ERROR: mfloop.py unexpectedly quit, waiting %waittime% seconds to restart...
 timeout /T %waittime% > NUL
